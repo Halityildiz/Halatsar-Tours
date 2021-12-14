8bfdb4d340f98e9de62540821c9d24da1a9f1f7e
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-15 02:01:57 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Tours from "./Tours";
+import { data } from "./Data";
+function App() {
+  // const tours = {data}
+  const [tours, setTours] = useState(data);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => setTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// // import Tour from "./Tour";
+// const Header = ({ tour, refreshTours }) => {
+//   return (
+//     <section>
+//       <div className="title">
+//         <h2>our tours</h2>
+//         <div className="underline"></div>
+//         <button className="btn" onClick={() => refreshTours()}>
+//           refresh
+//         </button>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Header;
